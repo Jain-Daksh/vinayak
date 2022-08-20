409edd44070c0f4ed76fcbee25ba504a253b2034
Jain-Daksh <daksh44jain@gmail.com>
2022-08-20 16:00:51 +0530

    product

--- components/Product.js
+++ components/Product.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Image from 'next/image'
-import Image1 from '../public/pngwing.com (6).png'
+import Image1 from '../public/bookshelf1.png'
 import Image2 from '../public/test.png'
-import Image3 from '../public/pngwing.com (10).png'
-import Image4 from '../public/pngwing.com (3).png'
-import Image5 from '../public/pngwing.com (4).png'
-import Image6 from '../public/pngwing.com (9).png'
-import Image7 from '../public/Colored-Almirah-PNG-Pic.png'
+import Image3 from '../public/pngwing.com (3).png'
+import Image4 from '../public/almari3.png'
+import Image5 from '../public/chair.webp'
+import Image6 from '../public/schooltable.jpg'
+import Image7 from '../public/stool.png'
+import Image8 from '../public/rack1.jpg'
+
 function Product() {
   return (
     <div>
@@ -28,45 +30,44 @@ function Product() {
 
               </div>
               <div className="row-cols-1 col-md-3 .col-sm-12">
-                <Image src={Image2} alt="logo" className='imagegallerys'></Image>
-                <figcaption>locker</figcaption>
-
-              </div>
-              <div className="row-cols-1 col-md-3 .col-sm-12">
-                <Image src={Image3} alt="logo" className='imagegallerys'></Image>
-                <figcaption>Office locker.</figcaption>
-
-              </div>
-              <div className="row-cols-1 col-md-3 .col-sm-12">
-                <Image src={Image4} alt="logo" className='imagegallerys'></Image>
-                <figcaption>Staff locker</figcaption>
-
-              </div>
-            </div>
-            <div className="row">
-              <div className="row-cols-1 col-md-3 .col-sm-12">
                 <Image src={Image5} alt="logo" className='imagegallerys'></Image>
-                <figcaption>Bunker Bed </figcaption>
-
-              </div>
-              <div className="row-cols-1 col-md-3 .col-sm-12">
-                <Image src={Image6} alt="logo" className='imagegallerys'></Image>
                 <figcaption>Chair</figcaption>
 
               </div>
               <div className="row-cols-1 col-md-3 .col-sm-12">
                 <Image src={Image7} alt="logo" className='imagegallerys'></Image>
-                <figcaption>Almirah</figcaption>
+                <figcaption>Stool</figcaption>
 
               </div>
               <div className="row-cols-1 col-md-3 .col-sm-12">
+                <Image src={Image4} alt="logo" className='imagegallerys'></Image>
+                <figcaption>Almirah</figcaption>
+
+              </div>
+            </div>
+            <div className="row">
+              <div className="row-cols-1 col-md-3 .col-sm-12">
                 <Image src={Image2} alt="logo" className='imagegallerys'></Image>
-                <figcaption>locker</figcaption>
+                <figcaption>Locker </figcaption>
+
+              </div>
+              <div className="row-cols-1 col-md-3 .col-sm-12">
+                <Image src={Image8} alt="logo" className='imagegallerys'></Image>
+                <figcaption>Rack</figcaption>
+
+              </div>
+              <div className="row-cols-1 col-md-3 .col-sm-12">
+                <Image src={Image3} alt="logo" className='imagegallerys'></Image>
+                <figcaption>Office Locker</figcaption>
+
+              </div>
+              <div className="row-cols-1 col-md-3 .col-sm-12">
+                <Image src={Image6} alt="logo" className='imagegallerys'></Image>
+                <figcaption>School Table</figcaption>
               </div>
             </div>
           </div>
         </div>
-
       </section>
     </div>
   )
