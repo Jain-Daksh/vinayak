cbea3075dd4e041ef02e6d7ed5e514cf8ae69619
Jain-Daksh <daksh44jain@gmail.com>
2022-08-22 18:25:38 +0530

    changed about us

--- components/AboutUs.js
+++ components/AboutUs.js
@@ -14,7 +14,7 @@ function AboutUs() {
                 Office Furniture. With the help of in-depth knowledge and rich experience good workmanship
                 Vinayak Steel Furniture has found a strong foothold in Udaipur Furniture Market since 2006. </p>
               <p className="about-text-para">Steel furniture has become an ideal choice in modern times as it brings solidity, modernity, and a clean appearance to the room. Just because the steel and stainless steel furniture do not lose their sheen for years, it becomes a popular choice among many using it to beautify their home décor.
-                With free maintenance, easy to wash, stain-resistant, and hygienic, the steel furniture comes in an extensive and impressive range of furniture, modular furniture, and home furnishings.</p>
+                With easy to wash, stain-resistant, and hygienic, the steel furniture comes in an extensive and impressive range of furniture, modular furniture, and home furnishings.</p>
             </div>
           </div>
         </div>
