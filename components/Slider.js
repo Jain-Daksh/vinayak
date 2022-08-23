45f3a411a501cc7fa40d166422c794812eadc44c
Jain-Daksh <daksh44jain@gmail.com>
2022-08-23 19:48:05 +0530

    fixed slider

--- components/Slider.js
+++ components/Slider.js
@@ -1,3 +1,4 @@
+/*
 import React from "react";
 import Image from 'next/image'
 import Image1 from '../public/table.jpg'
@@ -40,6 +41,80 @@ export default function Slider() {
         <Image src={Image6} alt="logo" className='imagegallerys'></Image>
         </div>
         </OwlCarousel>
+      </section>
+    </div>
+  );
+}
+*/
+
+
+import React from "react";
+import Image from 'next/image'
+import Image1 from '../public/table.jpg'
+import Image5 from '../public/rack1.jpg'
+import Image3 from '../public/schooltable.jpg'
+import Image2 from '../public/stool.webp'
+import Image6 from '../public/chair.webp'
+
+var $ = require("jquery");
+if (typeof window !== "undefined") {
+  // Client-side-only code
+  window.$ = window.jQuery = require("jquery");
+}
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
+import dynamic from "next/dynamic";
+
+const OwlCarousel = dynamic(() => import("react-owl-carousel"), {
+  ssr: false,
+});
+
+export default function Slider() {
+  return (
+    <div>
+      <section className="slidername">
+      <div className="row ">
+            
+            <OwlCarousel>
+            <div className="col-12 col-md-6 image-shops">
+                  <Image
+                    src={Image1}
+                    alt="Abc logo"
+                    className="image-shop"
+                  />
+                </div>
+                <div className="col-12 col-md-6 image-shops">
+                  <Image
+                    src={Image5}
+                    alt="Abc logo"
+                    className="image-shop"
+                  />
+                </div>
+                <div className="col-12 col-md-6 image-shops">
+                  <Image
+                    src={Image3}
+                    alt="Abc logo"
+                    className="image-shop"
+                  />
+                </div>
+                <div className="col-12 col-md-6 image-shops">
+                  <Image
+                    src={Image2}
+                    alt="Abc logo"
+                    className="image-shop"
+                  />
+                </div>
+                <div className="col-12 col-md-6 image-shops">
+                  <Image
+                    src={Image6}
+                    alt="Abc logo"
+                    className="image-shop"
+                  />
+                </div>
+                </OwlCarousel>
+
+               
+                </div>
       </section>
     </div>
   );
